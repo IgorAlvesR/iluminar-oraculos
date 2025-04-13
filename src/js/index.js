b70d978444fbc016022b363c395fbd84320fa63e
Igor Alves <igorservicosdev@gmail.com>
2025-04-12 23:44:28 -0300

    feat: inserir animação no menu.

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,19 @@ const header = document.querySelector('header')
 const navMenu = document.querySelector('#nav-menu')
 
 menuMobile.addEventListener('click', () => {
-  navMenu.classList.toggle('hidden')
+  navMenu.dataset.open = navMenu.dataset.open === 'true' ? 'false' : 'true'
+  const isOpen = !!(navMenu.dataset.open === 'true')
+
+  if (isOpen) {
+    navMenu.classList.remove('translate-y-[-500px]')
+    navMenu.classList.add('translate-y-0')
+  }
+  if (!isOpen) {
+    navMenu.classList.remove('translate-y-0')
+    navMenu.classList.add('translate-y-[-500px]')
+  }
   navMenu.classList.toggle('flex')
   header.classList.toggle('bg-primary')
+
+
 })
