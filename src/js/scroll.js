7adbc0f9205a6f121e373b9572939be0f648af3c
Igor Alves <igorservicosdev@gmail.com>
2025-05-04 20:33:15 -0300

    feat: criar seção de quem sou eu.

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -5,3 +5,7 @@ ScrollReveal().reveal('.main-image', { delay: 600, origin: 'right' });
 
 ScrollReveal().reveal('.price-title', { delay: 300 });
 ScrollReveal().reveal('.price-cards', { delay: 600 });
+
+ScrollReveal().reveal('.quem-sou-eu__titulo', { delay: 300, origin: 'top' });
+ScrollReveal().reveal('.quem-sou-eu__img', { delay: 600, origin: 'left' });
+ScrollReveal().reveal('.quem-sou-eu__conteudo', { delay: 600, origin: 'right' });
