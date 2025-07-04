9f3bdd1c55eca76e51294c7a186c84c7473ef59c
Igor Alves <igorservicosdev@gmail.com>
2025-07-04 16:08:23 -0300

    feat: inserir link para agendamento

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -1,3 +1,3 @@
 ScrollReveal({ distance: '60px', reset: true });
-ScrollReveal().reveal('#titulo-principal', { delay: 300, origin: 'left' });
-ScrollReveal().reveal('#imagem-cartas', { delay: 600, origin: 'right' });
+ScrollReveal().reveal('#titulo-principal', { delay: 300, duration: 1500, origin: 'left' });
+ScrollReveal().reveal('#imagem-cartas', { delay: 600, duration: 1500, origin: 'right' });
