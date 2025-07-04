c9ed2d7c91bfe943fe3044a2e68678b3402ea980
Igor Alves <igorservicosdev@gmail.com>
2025-07-04 16:42:40 -0300

    feat: inserir blur ao abrir menu

--- src/js/index.js
+++ src/js/index.js
@@ -5,14 +5,19 @@ const navMenu = document.querySelector('#nav-menu')
 menuMobile.addEventListener('click', () => {
   navMenu.dataset.open = navMenu.dataset.open === 'true' ? 'false' : 'true'
   const isOpen = !!(navMenu.dataset.open === 'true')
+  const mainSection = document.querySelector('#main')
 
   if (isOpen) {
     navMenu.classList.remove('translate-y-[-500px]')
-    navMenu.classList.add('translate-y-0', 'h-auto', 'absolute', 'top-[72px]', 'bg-primary', 'w-full')
+    navMenu.classList.add('translate-y-0', 'h-auto', 'absolute', 'top-[72px]', 'w-full')
+    document.body.classList.add('overflow-hidden')
+    mainSection.classList.add('blur-2xl')
   }
   if (!isOpen) {
+    document.body.classList.remove('overflow-hidden')
     navMenu.classList.remove('translate-y-0')
     navMenu.classList.add('translate-y-[-500px]')
+    mainSection.classList.remove('blur-2xl')
   }
   navMenu.classList.toggle('flex', 'h-0')
   header.classList.toggle('bg-primary')
